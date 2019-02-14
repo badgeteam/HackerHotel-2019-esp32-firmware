5bf088f7ec6bff7c08b4212405d018457fb9bdc0
Bug Blue <mendel@hack42.nl>
2019-02-14 20:49:18 +0100

    Pin nummer

--- components/badge/badge_pins.h
+++ components/badge/badge_pins.h
@@ -228,7 +228,7 @@
 		#define MPR121_PIN_NUM_UP        3
 		#define MPR121_PIN_NUM_LEFT      4
 		// TODO below
-		#define PIN_NUM_VIBRATOR  8
+		#define PIN_NUM_VIBRATOR  4
 		#define MPR121_PIN_NUM_CHRGSTAT  9
 		#define MPR121_PIN_NUM_LEDS     10
 		#define IIS_SCLK 13
