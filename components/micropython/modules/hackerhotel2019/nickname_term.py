1a4da8157877f66db7571669a3ab936634fee4d6
Renze Nicolai <renze@rnplus.nl>
2019-02-15 09:38:26 +0100

    Add downloader tool, fix bugs in WiFi

--- components/micropython/modules/hackerhotel2019/nickname_term.py
+++ components/micropython/modules/hackerhotel2019/nickname_term.py
@@ -1,9 +1,1 @@
-import term, appglue, sys, badge, easydraw
-
-easydraw.msg("This app can only be controlled using the USB-serial connection.", "Notice", True)
-
-term.header(True, "Configure nickname")
-nickname = badge.nvs_get_str("owner", "name", "")
-nickname = term.prompt("Nickname", 1, 3, nickname)
-badge.nvs_set_str("owner", "name", nickname)
-appglue.home()
+import dashboard.terminal.nickname
--- components/micropython/modules/hackerhotel2019/term_menu.py
+++ components/micropython/modules/hackerhotel2019/term_menu.py
@@ -21,8 +21,8 @@ class UartMenu():
 		import shell
 	
 	def menu_main(self):
-		items = ["Python shell", "Apps", "Installer", "Settings", "About", "Check for updates", self.power_off_label]
-		callbacks = [self.drop_to_shell, self.opt_launcher, self.opt_installer, self.menu_settings, self.opt_about, self.opt_ota_check, self.go_to_sleep]
+		items = ["Python shell", "Apps", "Installer", "Settings", "Tools", "About", "Check for updates", self.power_off_label]
+		callbacks = [self.drop_to_shell, self.opt_launcher, self.opt_installer, self.menu_settings, self.menu_tools, self.opt_about, self.opt_ota_check, self.go_to_sleep]
 		message = "Welcome!\nYour badge is running firmware version "+str(version.build)+": "+version.name+"\n"
 		cb = term.menu("Main menu", items, 0, message)
 		self.menu = callbacks[cb]
@@ -32,7 +32,7 @@ class UartMenu():
 		self.gts()
 		
 	def opt_change_nickname(self):
-		system.start("nickname_term", True)
+		system.start("dashboard.terminal.nickname", True)
 		
 	def opt_installer(self):
 		system.start("dashboard.terminal.installer", True)
@@ -64,14 +64,23 @@ class UartMenu():
 	def opt_configure_picture(self):
 		system.start("dashboard.terminal.picture", True)
 		
+	def opt_downloader(self):
+		system.start("dashboard.terminal.downloader", True)
+		
 	def menu_settings(self):
 		items = ["Change nickname", "Configure WiFi", "Set default orientation", "Homescreen services (beta)", "Homescreen LED animation", "Homescreen logo", "Update firmware", "< Return to main menu"]
 		callbacks = [self.opt_change_nickname, self.opt_configure_wifi, self.opt_configure_orientation, self.opt_configure_services, self.opt_configure_led, self.opt_configure_picture, self.opt_ota, self.menu_main, self.menu_main]
 		cb = term.menu("Settings", items)
 		self.menu = callbacks[cb]
 	
+	def menu_tools(self):
+		items = ["File downloader", "< Return to main menu"]
+		callbacks = [self.opt_downloader, self.menu_main, self.menu_main]
+		cb = term.menu("Tools", items)
+		self.menu = callbacks[cb]
+	
 	def menu_safe(self):
 		items = ["Main menu"]
 		callbacks = [self.menu_main]
-		cb = term.menu("You have started the badge in safe mode.\nServices are disabled.", items)
+		cb = term.menu("You have started the badge in safe mode!", items)
 		self.menu = callbacks[cb]
